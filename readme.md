4618b27eea18a16baa5a3ff976a019c211165e2f
William Villeneuve <ciiqr@users.noreply.github.com>
2018-04-03 00:03:30 -0400

    added a bunch of aur packages, improved build scripts

--- readme.md
+++ readme.md
@@ -11,6 +11,7 @@
 ...
 [monokaiba]
 Server = https://github.com/ciiqr/repo-arch/releases/download/v2018.04.01.19.37.45
+# or from local, ie. Server = file:///vagrant/.build/monokaiba
 SigLevel = PackageOptional
 ```
 
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-pacman -S --noconfirm base-devel namcap hub
+pacman -S --noconfirm base-devel namcap hub help2man
+
+# for ncurses5-compat-libs
+gpg --keyserver keys.gnupg.net --recv-keys 702353E0F7E48EDB
+
+# for swift-bin
+gpg --keyserver keys.gnupg.net --recv-keys EF5430F071E1B235
